fcfd1d040eb5c8fadf40728dfa94308f6b001bf8
Ankit Varma <167162564+AnkitFifteen@users.noreply.github.com>
2024-04-24 04:32:22 +0000

    modified ModuleExample1

--- IPYNB/ModuleExample1/Area.py
+++ IPYNB/ModuleExample1/Area.py
@@ -2,21 +2,21 @@ import math
 
 def square():
     side = float(input("Enter side of square: "))
-    print("Result: ", side ** 2)
+    print("Area of square: ", side ** 2)
 
 def rectangle():
     length = float(input("Enter length of rectangle: "))
     breadth = float(input("Enter breadth of rectangle: "))
-    print("Result: ", length * breadth)
+    print("Area of rectangle: ", length * breadth)
 
 def triangle():
     base = float(input("Enter base of triangle: "))
     height = float(input("Enter height of triangle: "))
-    print("Result: ", (base * height)/2)
+    print("Area of triangle: ", (base * height)/2)
 
 def circle():
     radius = float(input("Enter radius of circle: "))
-    print("Result: ", math.pi * (radius**2))
+    print("Area of circle: ", math.pi * (radius**2))
 
 def start():
     options = str(input("What do you want to perform?\n #1 Area of Square Calculation\n #2 Area of Rectangle Calculation\n #3 Area of Triangle Calculation\n #4 Area of Circle Calculation\nEnter choice number: "))
--- IPYNB/ModuleExample1/Conversion.py
+++ IPYNB/ModuleExample1/Conversion.py
@@ -1,14 +1,14 @@
 def cm_to_meter():
     cm = float(input("Enter cm: "))
-    print(cm + " centimetres in metres is: ", cm/100)
+    print(cm, "centimetres in metres is:", cm/100)
 
 def inch_to_cm():
     inch = float(input("Enter inch: "))
-    print(inch + " inch in centimetres is: ", inch * 2.54)
+    print(inch, "inch in centimetres is:", inch * 2.54)
 
 def fahrenheit_to_celsius():
     fahrenheit = float(input("Enter fahrenheit: "))
-    print(fahrenheit + " fahrenheit in celsius is: ", (fahrenheit- 32) * 5/9)
+    print(fahrenheit, "fahrenheit in celsius is:", (fahrenheit- 32) * 5/9)
 
 def start():
     options = str(input("What do you want to perform?\n #1 Centimetre to Meter Calculation\n #2 Inch to Centimetre Calculation\n #3 Fahrenheit to Celsius Calculation\nEnter choice number: "))
