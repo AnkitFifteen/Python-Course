300cf383561b130868c0336a3b62db9110df03d3
Ankit Varma <167162564+AnkitFifteen@users.noreply.github.com>
2024-04-24 05:14:00 +0000

    modified file operations

--- IPYNB/FileOperations/Operations.py
+++ IPYNB/FileOperations/Operations.py
@@ -1,8 +1,19 @@
-f = open("demo.txt", "r")
+import os
+
+filePath = "IPYNB/FileOperations/demo.txt"
+f = open(filePath, "r")
 print(f.read())
 print(f.readline())
 for i in f:
     print(i)
 f.close()
 
-f = open("demo.txt", "w")
+f = open(filePath, "w")
+f.write("this is the new content of file. Previous content is now deleted.")
+f.close()
+
+f = open(filePath, "a")
+f.write("this is the new content of file. Previous content is not deleted.")
+f.close()
+
+os.remove(filePath)
