49f54a14a1f28d6aa3105be303ac22b117c5e5ac
Ankit Varma <167162564+AnkitFifteen@users.noreply.github.com>
2024-04-25 04:19:59 +0000

    added another operator

--- Codes/FileOperations/Operations.py
+++ Codes/FileOperations/Operations.py
@@ -1,19 +1,31 @@
 import os
 
-filePath = "IPYNB/FileOperations/demo.txt"
-f = open(filePath, "r")
-print(f.read())
-print(f.readline())
-for i in f:
-    print(i)
-f.close()
+demoFilePath = "Codes/FileOperations/demo.txt"
+sample_textFilePath = "Codes/FileOperations/sample_text.txt"
 
-f = open(filePath, "w")
-f.write("this is the new content of file. Previous content is now deleted.")
-f.close()
+def demo_operator():
+    f = open(demoFilePath, "r")
+    print(f.read())
+    print(f.readline())
+    for i in f:
+        print(i)
+    f.close()
 
-f = open(filePath, "a")
-f.write("this is the new content of file. Previous content is not deleted.")
-f.close()
+    f = open(demoFilePath, "w")
+    f.write("this is the new content of file. Previous content is now deleted.")
+    f.close()
 
-os.remove(filePath)
+    f = open(demoFilePath, "a")
+    f.write("this is the new content of file. Previous content is not deleted.")
+    f.close()
+    os.remove(demoFilePath)
+
+def sample_text_operator():
+    occurrence = 0
+    word_to_search = str(input("Enter the word to search for: "))
+    fileContents = open(sample_textFilePath, "r").read()
+    words_list = fileContents.split(" ")
+    occurrence = words_list.count(word_to_search)
+    print("Occurrence of", word_to_search, "in file =", occurrence)
+
+sample_text_operator()
