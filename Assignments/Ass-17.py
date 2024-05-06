ba3ca89720f9a76cf844b3663a11848238a8476a
Ankit Varma <167162564+AnkitFifteen@users.noreply.github.com>
2024-05-06 09:16:07 +0000

    added Ass-17

---
+++ Assignments/Ass-17.py
@@ -0,0 +1,25 @@
+'''
+Create a class called library with data attributes like acc_number, publisher, title
+
+and author. The methods of the class should include
+
+a. read() – acc_number, title, author.
+
+b. compute() - to accept the number of days late, calculate and display the fine
+
+charged at the rate of $1.50 per day.
+
+c. display the data.
+'''
+
+class library:
+    acc_number = 000
+    publisher = "sdf"
+    title = "asd"
+    author = "adfas"
+
+    def read(self):
+        print(f"Account Number: {self.acc_number} Publisher: {self.publisher} Title: {self.title} Author: {self.author}")
+
+    def compute():
+        
