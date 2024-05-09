71d878c55825054413fecb6ffccc33ae5b2a66a4
Ankit Varma <167162564+AnkitFifteen@users.noreply.github.com>
2024-05-09 14:50:44 +0530

    Update Ass-17.py

--- Assignments/Ass-17.py
+++ Assignments/Ass-17.py
@@ -1,25 +1,40 @@
-'''
-Create a class called library with data attributes like acc_number, publisher, title
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  9 14:35:12 2024
 
+@author: ankit
+
+Create a class called library with data attributes like acc_number, publisher, title
 and author. The methods of the class should include
 
 a. read() – acc_number, title, author.
-
 b. compute() - to accept the number of days late, calculate and display the fine
-
-charged at the rate of $1.50 per day.
-
+   charged at the rate of $1.50 per day.
 c. display the data.
-'''
+"""
 
 class library:
     acc_number = 000
-    publisher = "sdf"
-    title = "asd"
-    author = "adfas"
+    publisher = "unknown"
+    title = "unknown"
+    author = "unknown"
 
-    def read(self):
-        print(f"Account Number: {self.acc_number} Publisher: {self.publisher} Title: {self.title} Author: {self.author}")
+    def read(self, AccountNumber, Publisher, Title, Author):
+        self.acc_number = AccountNumber
+        self.publisher = Publisher
+        self.title = Title
+        self.author = Author
 
-    def compute():
+    def compute(self):
+        noOfDaysLate = int(input("Enter no. of days late: "))
+        rate = 1.50
+        print(f"Fine to be charged = ${rate * noOfDaysLate}")
         
+    def display(self):
+        print(f"Account Number: {self.acc_number} Publisher: {self.publisher} Title: {self.title} Author: {self.author}")
+        
+Lib1 = library()
+Lib1.read(258, "McGraw Hill", "Core Java", "James")
+Lib1.compute()
+Lib1.display()
