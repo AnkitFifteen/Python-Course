737b0c369162ecc4daf81b2c214d0d04712b237d
Ankit Varma <167162564+AnkitFifteen@users.noreply.github.com>
2024-05-16 04:54:11 +0000

    added greeting

--- DjangoWebApps/WeatherGreetingFolder/WeatherGreetingProject/WeatherGreetingApp/views.py
+++ DjangoWebApps/WeatherGreetingFolder/WeatherGreetingProject/WeatherGreetingApp/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from datetime import datetime, date, time, timezone
 # Create your views here.
+
+def greeting1(request):
+    dt = datetime.now()
+    TimeTuple = dt.timetuple()
+    index = 0
+    for value in TimeTuple:
+        print(value)
+        index += 1
+    t = time.hour
+    resp2 = HttpResponse("Today's date and time %s" %t)
+    return resp2
+
+def greeting(request):
+    dt = datetime.now()
+    msg = "unknown"
+    if dt.hour < 12:
+        msg = "Good Morning"
+    elif dt.hour < 16 and dt.hour > 12:
+        msg = "Good Afternoon"
+    else:
+        msg = "Good Evening"
+    return HttpResponse(msg)
