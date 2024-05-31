472f844a598ab3eee1c223bdeea91df1a244e402
Ankit Varma <167162564+AnkitFifteen@users.noreply.github.com>
2024-05-31 04:03:00 +0000

    Add images of pets

--- DjangoWebApps/PetStoreFolder/PetStoreProject/PetStoreProject/urls.py
+++ DjangoWebApps/PetStoreFolder/PetStoreProject/PetStoreProject/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from PetStoreProject import settings
+from django.conf import settings
 from django.conf.urls.static import static
 from PetStoreApp.views import ViewPets
 
@@ -27,4 +27,4 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
