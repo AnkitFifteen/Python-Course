1ced8b66160b967e270758d454a2acd44f6f0503
Ankit Varma <167162564+AnkitFifteen@users.noreply.github.com>
2024-06-02 05:36:00 +0000

    Done with IndianStatesApp

--- DjangoWebApps/IndianStatesFolder/IndianStatesProject/IndianStatesApp/models.py
+++ DjangoWebApps/IndianStatesFolder/IndianStatesProject/IndianStatesApp/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class State(models.Model):
-    name = models.CharField(max_length=100)
-    population = models.BigIntegerField()
-    language = models.CharField(max_length=100)
-    capital = models.CharField(max_length=100)
+    Name = models.CharField(max_length=100)
+    Population = models.BigIntegerField()
+    Language = models.CharField(max_length=100)
+    Capital = models.CharField(max_length=100)
 
     class Meta:
         db_table = "State"
